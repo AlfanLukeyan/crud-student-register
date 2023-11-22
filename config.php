37e263c7a491aa230c4426519bf5ad20e30bff14
Alfan Lukeyan Rizki <alfanlukeyanrizki@gmail.com>
2023-11-22 23:44:06 +0700

    fix: project

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
-$host = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$database = "pendaftaran_siswa"; 
 
-$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
+$server = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran-siswa";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+
 ?>
--- index.php
+++ index.php
@@ -1,46 +1,34 @@
-<html>
-<head>
-  <title>Aplikasi CRUD Plus Upload Gambar dengan PHP</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <section class="title">
-    <h1>Data Siswa</h1>
-    <a href="form_simpan.php" class="button">Tambah Data</a>
-  </section>
-  <table border="1" width="100%">
-  <thead>
-    <tr>
-      <th>Foto</th>
-      <th>NIS</th>
-      <th>Nama</th>
-      <th>Jenis Kelamin</th>
-      <th>Telepon</th>
-      <th>Alamat</th>
-      <th colspan="2">Aksi</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-  include "config.php";
-
-  $sql = $pdo->prepare("SELECT * FROM siswa");
-  $sql->execute(); 
-
-  while($data = $sql->fetch()){ 
-    echo "<tr>";
-    echo "<td><img src='images/".$data['foto']."' width='100' height='100'></td>";
-    echo "<td>".$data['nis']."</td>";
-    echo "<td>".$data['nama']."</td>";
-    echo "<td>".$data['jenis_kelamin']."</td>";
-    echo "<td>".$data['telp']."</td>";
-    echo "<td>".$data['alamat']."</td>";
-    echo "<td><a href='form_ubah.php?id=".$data['id']."' class='button'>Ubah</a></td>";
-    echo "<td><a href='proses_hapus.php?id=".$data['id']."' class='button'>Hapus</a></td>";
-    echo "</tr>";
-  }
-  ?>
-  </tbody>
-  </table>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link rel="stylesheet" href="css/lmao.css" />
+  </head>
+  <body>
+    <div class="login-page">
+    <div class="menu">
+    <h1>Pendaftaran Siswa Baru</h1>
+    <h1>SMA Coding</h1>
+    </div>
+      <div class="form">
+        <h2>Menu</h2>
+        <a href="form-daftar.php">Daftar Baru</a>
+        <a href="list-siswa.php">Pendaftar</a>
+        <?php if(isset($_GET['status'])): ?>
+            <p>
+                <?php
+                    if($_GET['status'] == 'sukses'){
+                        echo "Pendaftaran siswa baru berhasil!";
+                    } else {
+                        echo "Pendaftaran gagal!";
+                    }
+                ?>
+            </p>
+        <?php endif; ?>
+      </div>
+    </div>
+  </body>
 </html>
